49109621e00d11bece3adb8e34d1a536a15ec06d
Melissa Perez <melmperz@gmail.com>
2021-11-29 23:37:54 -0500

    Add Table and TableHead

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,1 +1,18 @@
-impo;
+import React from "react";
+import TableHead from "../components/TableHead";
+import Row from "../components/Row";
+
+function Table({ exercises }) {
+  return (
+    <table id="exercises">
+      <TableHead></TableHead>
+      <tbody>
+        {exercises.map((exercise, i) => (
+          <Exercise exercise={exercise} key={i} />
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default Table;
--- src/components/TableHead.js
+++ src/components/TableHead.js
@@ -7,17 +7,17 @@ import React from "react";
 */
 function TableHead() {
   return (
-    <>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Reps</th>
-          <th>Weight</th>
-          <th>Unit</th>
-          <th>Date</th>
-        </tr>
-      </thead>
-    </>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Reps</th>
+        <th>Weight</th>
+        <th>Unit</th>
+        <th>Date</th>
+        <th>Edit</th>
+        <th>Delete</th>
+      </tr>
+    </thead>
   );
 }
 
