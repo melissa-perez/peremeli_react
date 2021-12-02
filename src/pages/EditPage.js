8c68f8fd1fe609daeffaac6d6c8da45ef71afbc8
Melissa Perez <melmperz@gmail.com>
2021-12-01 20:19:22 -0500

    Work on EditPage

---
+++ src/pages/EditPage.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+export const EditPage = ({ exerciseToEdit }) => {
+  const [name, setName] = useState(exerciseToEdit.name);
+  const [reps, setReps] = useState(exerciseToEdit.reps);
+  const [weight, setWeight] = useState(exerciseToEdit.weight);
+  const [unit, setUnit] = useState(exerciseToEdit.unit);
+  const [date, setDate] = useState(exerciseToEdit.date);
+
+  const history = useHistory();
+
+  const editExercise = async () => {
+    const response = await fetch(`/exercises/${exerciseToEdit}`, {
+      method: "PUT",
+      body: JSON.stringify({ name, reps, weight, unit, date }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.status === 201) {
+      alert("Successfully edited the exercise!");
+    } else {
+      alert(`Failed to edit exercise, status code = ${response.status}`);
+    }
+    history.push("/");
+  };
+  return (
+    <div>
+      <h2>Edit Exercise</h2>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="number"
+        value={reps}
+        min="0"
+        onChange={(e) => setReps(e.target.value)}
+      />
+      <input
+        type="number"
+        min="0"
+        value={weight}
+        onChange={(e) => setWeight(e.target.value)}
+      />
+      <input
+        type="text"
+        value={unit}
+        onChange={(e) => setUnit(e.target.value)}
+      />
+      <input
+        type="date"
+        value={date}
+        onChange={(e) => setDate(e.target.value)}
+      />
+      <button onClick={editExercise}>Save</button>
+    </div>
+  );
+};
+
+export default EditPage;
